ff3ed94645df1d3a6c94143cc532d0d23c6b7bb8
tarun1475 <tarunkumargupta14@gmail.com>
2020-05-04 20:22:07 +0530

    beta net deployment

--- src/truffle-config.js
+++ src/truffle-config.js
@@ -55,6 +55,14 @@ module.exports = {
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
+    maticBetaMainnet: {
+      provider: new HDWalletProvider(mnemonic, `https://betav2.matic.network`),
+      network_id: 16110,       // Matic's test network id
+      gas: 7000000,
+      confirmations: 1,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
     ropsten: {
       provider: new HDWalletProvider(mnemonic, `https://ropsten.infura.io/v3/${infuraKey}`),
       network_id: 3,       // Ropsten's id

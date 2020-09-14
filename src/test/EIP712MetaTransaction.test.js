8c2636f62148696c05528982b368ec321cc300b0
divya <divya@biconomy.io>
2020-09-14 12:43:22 +0530

    Comments resolved. More test scenerios added

--- src/test/EIP712MetaTransaction.test.js
+++ src/test/EIP712MetaTransaction.test.js
@@ -3,31 +3,66 @@ const TestContract = artifacts.require("TestContract");
 const web3Abi = require('web3-eth-abi');
 const sigUtil = require('eth-sig-util');
 
-contract("EIP712MetaTransaction", function([_, owner,account1, implementationAddress, chiTokenAddress, notOwner, destination]) {
+let ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
+let publicKey = "0x726cDa2Ac26CeE89F645e55b78167203cAE5410E";
+let privateKey = "0x68619b8adb206de04f676007b2437f99ff6129b672495a6951499c6c56bc2fa6";
+let executeMetaTransactionABI = { "constant": false, "inputs": [ { "internalType": "address", "name": "userAddress", "type": "address" }, { "internalType": "bytes", "name": "functionSignature", "type": "bytes" }, { "internalType": "bytes32", "name": "sigR", "type": "bytes32" }, { "internalType": "bytes32", "name": "sigS", "type": "bytes32" }, { "internalType": "uint8", "name": "sigV", "type": "uint8" } ], "name": "executeMetaTransaction", "outputs": [ { "internalType": "bytes", "name": "", "type": "bytes" } ], "payable": true, "stateMutability": "payable", "type": "function" };
+let setQuoteAbi = { "constant": false, "inputs": [ { "internalType": "string", "name": "newQuote", "type": "string" } ], "name": "setQuote", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" };
+let quoteToBeSet = "Divya";
 
-    let publicKey = "0x726cDa2Ac26CeE89F645e55b78167203cAE5410E";
-    let privateKey = "0x68619b8adb206de04f676007b2437f99ff6129b672495a6951499c6c56bc2fa6";
-	let eip712MetaTransaction;
+const domainType = [
+    { name: "name", type: "string" },
+    { name: "version", type: "string" },
+    { name: "chainId", type: "uint256" },
+    { name: "verifyingContract", type: "address" }
+];
+
+const metaTransactionType = [
+    { name: "nonce", type: "uint256" },
+    { name: "from", type: "address" },
+    { name: "functionSignature", type: "bytes" }
+];
+
+let domainData ;
+
+const returnTransactionData = async ( nonce ) => {
+    
+    const functionSignature = web3Abi.encodeFunctionCall(
+        setQuoteAbi,
+        [quoteToBeSet]
+    );
+
+    let message = {};
+    message.nonce = parseInt(nonce);
+    message.from = publicKey;
+    message.functionSignature = functionSignature;
+
+    const dataToSign = {
+        types: {
+            EIP712Domain: domainType,
+            MetaTransaction: metaTransactionType
+        },
+        domain: domainData,
+        primaryType: "MetaTransaction",
+        message: message
+    };
+    const signature = sigUtil.signTypedData_v4(new Buffer(privateKey.substring(2,66), 'hex'), { data: dataToSign });
+
+    let r = signature.slice(0, 66);
+    let s = "0x".concat(signature.slice(66, 130));
+    let v = "0x".concat(signature.slice(130, 132));
+    v = web3.utils.hexToNumber(v);
+    if (![27, 28].includes(v)) v += 27;
+
+    return {r, s, v, functionSignature};
+}
+
+contract("EIP712MetaTransaction", function([_, owner,account1 ]) {
+    
+    let eip712MetaTransaction;
     let testContract
-	let executeMetaTransactionABI = { "constant": false, "inputs": [ { "internalType": "address", "name": "userAddress", "type": "address" }, { "internalType": "bytes", "name": "functionSignature", "type": "bytes" }, { "internalType": "bytes32", "name": "sigR", "type": "bytes32" }, { "internalType": "bytes32", "name": "sigS", "type": "bytes32" }, { "internalType": "uint8", "name": "sigV", "type": "uint8" } ], "name": "executeMetaTransaction", "outputs": [ { "internalType": "bytes", "name": "", "type": "bytes" } ], "payable": true, "stateMutability": "payable", "type": "function" };
-    let setQuoteAbi = { "constant": false, "inputs": [ { "internalType": "string", "name": "newQuote", "type": "string" } ], "name": "setQuote", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" };
-    
-    const domainType = [
-        { name: "name", type: "string" },
-        { name: "version", type: "string" },
-        { name: "chainId", type: "uint256" },
-        { name: "verifyingContract", type: "address" }
-    ];
-    
-    const metaTransactionType = [
-        { name: "nonce", type: "uint256" },
-        { name: "from", type: "address" },
-        { name: "functionSignature", type: "bytes" }
-    ];
-    
-    let domainData ;
 
-	before('before', async function () {
+    before('before', async function () {
         eip712MetaTransaction = await EIP712MetaTransaction.new("TestContract", "1" , {from: owner});
         testContract = await TestContract.new({from: owner});
         domainData = {
@@ -38,42 +73,15 @@ contract("EIP712MetaTransaction", function([_, owner,account1, implementationAdd
         };
 	});
 
-	describe("Check MEthods", function(){
+	describe("Check Methods", function(){
 		it("Should be able to send transaction successfully", async()=>{
-            let quoteToBeSet = "Divya";
-            let nonce = await eip712MetaTransaction.getNonce(publicKey, {from : owner});
-            const functionSignature = web3Abi.encodeFunctionCall(
-				setQuoteAbi,
-				[quoteToBeSet]
-			);
+            let nonce = await testContract.getNonce(publicKey, {from : owner});
+            let { r, s, v, functionSignature} = await returnTransactionData( nonce );
 
-            let message = {};
-            message.nonce = parseInt(nonce);
-            message.from = publicKey;
-            message.functionSignature = functionSignature;
-
-            const dataToSign = {
-                types: {
-                    EIP712Domain: domainType,
-                    MetaTransaction: metaTransactionType
-                },
-                domain: domainData,
-                primaryType: "MetaTransaction",
-                message: message
-            };
-            const signature = sigUtil.signTypedData_v4(new Buffer(privateKey.substring(2,66), 'hex'), { data: dataToSign });
-
-            let r = signature.slice(0, 66);
-            let s = "0x".concat(signature.slice(66, 130));
-            let v = "0x".concat(signature.slice(130, 132));
-            v = web3.utils.hexToNumber(v);
-            if (![27, 28].includes(v)) v += 27;
-
-            const sendTransactionData = web3Abi.encodeFunctionCall(
+            let sendTransactionData = web3Abi.encodeFunctionCall(
 				executeMetaTransactionABI,
 				[publicKey, functionSignature, r, s, v]
             );
-            
             await testContract.sendTransaction({ 
 				value: 0, 
 				from: owner, 
@@ -87,34 +95,59 @@ contract("EIP712MetaTransaction", function([_, owner,account1, implementationAdd
             assert.isTrue(newQoute.currentQuote == quoteToBeSet, "Unable to set quote");
         });
 
+        it("Should fail when replay transaction", async()=>{
+            let nonce = await testContract.getNonce(publicKey, {from : owner});
+            let { r, s, v, functionSignature} = await returnTransactionData( nonce );
+
+            const sendTransactionData = web3Abi.encodeFunctionCall(
+				executeMetaTransactionABI,
+				[publicKey, functionSignature, r, s, v]
+            );
+            
+            await testContract.sendTransaction({ 
+				value: 0, 
+				from: owner, 
+				gas: 500000,
+				data: sendTransactionData
+            });
+
+            try{
+                await testContract.sendTransaction({ 
+                    value: 0, 
+                    from: owner, 
+                    gas: 500000,
+                    data: sendTransactionData
+                });
+            } catch (error){
+                assert.isTrue(error.message.includes("Signer and signature do not match"),`Wrong failure type`);
+            }
+        });
+
+        it("Should fail when user address is Zero", async()=>{
+            
+            let nonce = await testContract.getNonce(publicKey, {from : owner});
+            let { r, s, v, functionSignature} = await returnTransactionData( nonce );
+
+            const sendTransactionData = web3Abi.encodeFunctionCall(
+				executeMetaTransactionABI,
+				[ZERO_ADDRESS, functionSignature, r, s, v]
+            );
+
+            try{
+                await testContract.sendTransaction({ 
+                    value: 0, 
+                    from: owner, 
+                    gas: 500000,
+                    data: sendTransactionData
+                });
+            } catch (error){
+                assert.isTrue(error.message.includes("Signer and signature do not match"),`Wrong failure type`);
+            }
+        });
+
         it("Should be failed - Signer and Signature do not match", async()=>{
             let nonce = await eip712MetaTransaction.getNonce(publicKey, {from : owner});
-            const functionSignature = web3Abi.encodeFunctionCall(
-				setQuoteAbi,
-				["Divya"]
-			);
-
-            let message = {};
-            message.nonce = parseInt(nonce);
-            message.from = publicKey;
-            message.functionSignature = functionSignature;
-
-            const dataToSign = {
-                types: {
-                    EIP712Domain: domainType,
-                    MetaTransaction: metaTransactionType
-                },
-                domain: domainData,
-                primaryType: "MetaTransaction",
-                message: message
-            };
-            const signature = sigUtil.signTypedData_v4(new Buffer(privateKey.substring(2,66), 'hex'), { data: dataToSign });
-
-            let r = signature.slice(0, 66);
-            let s = "0x".concat(signature.slice(66, 130));
-            let v = "0x".concat(signature.slice(130, 132));
-            v = web3.utils.hexToNumber(v);
-            if (![27, 28].includes(v)) v += 27;
+            let { r, s, v, functionSignature} = await returnTransactionData( nonce );
 
             const sendTransactionData = web3Abi.encodeFunctionCall(
 				executeMetaTransactionABI,
@@ -129,7 +162,7 @@ contract("EIP712MetaTransaction", function([_, owner,account1, implementationAdd
                     data: sendTransactionData
                 });
             } catch (error){
-                assert.isTrue(error.message.includes("Signer and signature do not match"),`Wrong failure type, expected '${message}' but got '${error.message}'`);
+                assert.isTrue(error.message.includes("Signer and signature do not match"),`Wrong failure type`);
             }  
         });
 	});

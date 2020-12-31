5c33b154895a032d4b9227788e03eea74dcf1297
livingrock7 <chiragtitiya@exvillage.com>
2020-12-31 10:48:03 +0530

    test forwarder solidity code

---
+++ src/contracts/TestForwarder.sol
@@ -0,0 +1,25 @@
+pragma solidity ^0.6.0;
+
+import "https://github.com/opengsn/gsn/contracts/BaseRelayRecipient.sol";
+
+contract ForwarderTest is BaseRelayRecipient {
+
+    string public quote;
+    address public owner;
+    
+    constructor(address forwarder) public{
+        trustedForwarder = forwarder;
+    }
+
+    function setQuote(string memory newQuote) public {
+        quote = newQuote;
+        owner = _msgSender();
+    }
+
+    function getQuote() view public returns(string memory currentQuote, address currentOwner) {
+        currentQuote = quote;
+        currentOwner = owner;
+    }
+    
+    function versionRecipient() external virtual override view returns (string memory){return "1";}
+}

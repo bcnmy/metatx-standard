2bc00d618d9dc042292a7a221ac11a7b8315cba5
livingrock7 <chiragtitiya@exvillage.com>
2021-04-26 02:24:05 +0530

    moonbeam test demo

--- example/react-ui/src/App.js
+++ example/react-ui/src/App.js
@@ -3,54 +3,154 @@ import "./App.css";
 import Button from "@material-ui/core/Button";
 import {
   NotificationContainer,
-  NotificationManager
+  NotificationManager,
 } from "react-notifications";
 import "react-notifications/lib/notifications.css";
+import Backdrop from "@material-ui/core/Backdrop";
+import CircularProgress from "@material-ui/core/CircularProgress";
+
 import Web3 from "web3";
+//web3 has some issue regarding uint256 to bytes32
 import { Biconomy } from "@biconomy/mexa";
-import { makeStyles } from '@material-ui/core/styles';
-import Link from '@material-ui/core/Link';
-import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/core/styles";
+import Link from "@material-ui/core/Link";
+import Typography from "@material-ui/core/Typography";
 import { Box } from "@material-ui/core";
 let sigUtil = require("eth-sig-util");
-const { config } = require("./config");
+let config = {
+  contract: {
+    address: "0xe55348C7994f0f76CD0eB68003fD5A42025247a7",
+    abi: [
+      {
+        anonymous: false,
+        inputs: [
+          {
+            indexed: false,
+            internalType: "address",
+            name: "userAddress",
+            type: "address",
+          },
+          {
+            indexed: false,
+            internalType: "addresspayable",
+            name: "relayerAddress",
+            type: "address",
+          },
+          {
+            indexed: false,
+            internalType: "bytes",
+            name: "functionSignature",
+            type: "bytes",
+          },
+        ],
+        name: "MetaTransactionExecuted",
+        type: "event",
+      },
+      {
+        inputs: [
+          { internalType: "address", name: "userAddress", type: "address" },
+          { internalType: "bytes", name: "functionSignature", type: "bytes" },
+          { internalType: "bytes32", name: "sigR", type: "bytes32" },
+          { internalType: "bytes32", name: "sigS", type: "bytes32" },
+          { internalType: "uint8", name: "sigV", type: "uint8" },
+        ],
+        name: "executeMetaTransaction",
+        outputs: [{ internalType: "bytes", name: "", type: "bytes" }],
+        stateMutability: "payable",
+        type: "function",
+      },
+      {
+        inputs: [{ internalType: "address", name: "user", type: "address" }],
+        name: "getNonce",
+        outputs: [{ internalType: "uint256", name: "nonce", type: "uint256" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "getQuote",
+        outputs: [
+          { internalType: "string", name: "currentQuote", type: "string" },
+          { internalType: "address", name: "currentOwner", type: "address" },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "owner",
+        outputs: [{ internalType: "address", name: "", type: "address" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [],
+        name: "quote",
+        outputs: [{ internalType: "string", name: "", type: "string" }],
+        stateMutability: "view",
+        type: "function",
+      },
+      {
+        inputs: [{ internalType: "string", name: "newQuote", type: "string" }],
+        name: "setQuote",
+        outputs: [],
+        stateMutability: "nonpayable",
+        type: "function",
+      },
+    ],
+  },
+  apiKey: {
+    test: "wxPEpFFbg.774dfe00-b6a9-4d82-b6d8-52db802536a3",
+    prod: "8nvA_lM_Q.0424c54e-b4b2-4550-98c5-8b437d3118a9",
+  },
+};
 
 const domainType = [
   { name: "name", type: "string" },
   { name: "version", type: "string" },
-  { name: "chainId", type: "uint256" },
-  { name: "verifyingContract", type: "address" }
+  { name: "verifyingContract", type: "address" },
+  { name: "salt", type: "bytes32" },
 ];
 
 const metaTransactionType = [
   { name: "nonce", type: "uint256" },
   { name: "from", type: "address" },
-  { name: "functionSignature", type: "bytes" }
+  { name: "functionSignature", type: "bytes" },
 ];
 
 let domainData = {
   name: "TestContract",
   version: "1",
-  verifyingContract: config.contract.address
+  verifyingContract: config.contract.address,
+  salt: "0x" + (1287).toString(16).padStart(64, "0"),
 };
-
-let web3;
+let web3, walletWeb3;
 let contract;
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    '& > * + *': {
+    "& > * + *": {
       marginLeft: theme.spacing(2),
     },
   },
   link: {
-    marginLeft: "5px"
-  }
+    marginLeft: "5px",
+  },
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff",
+    opacity: ".85!important",
+    background: "#000",
+  },
 }));
 
 function App() {
   const classes = useStyles();
   const preventDefault = (event) => event.preventDefault();
+  const [backdropOpen, setBackdropOpen] = React.useState(true);
+  const [loadingMessage, setLoadingMessage] = React.useState(
+    " Loading Application ..."
+  );
   const [quote, setQuote] = useState("This is a default quote");
   const [owner, setOwner] = useState("Default Owner Address");
   const [newQuote, setNewQuote] = useState("");
@@ -65,14 +165,25 @@ function App() {
         window.ethereum.isMetaMask
       ) {
         // Ethereum user detected. You can now use the provider.
-          const provider = window["ethereum"];
-          await provider.enable();
-          if (provider.networkVersion == "80001") {
-            domainData.chainId = 80001;
-          const biconomy = new Biconomy(provider,{apiKey: "MublK0H-b.864389b3-e421-4ddb-b56b-e5a1bca3e109", debug: true});
-          web3 = new Web3(biconomy);
+        const provider = window["ethereum"];
+        await provider.enable();
+        let moonProvider = new Web3.providers.HttpProvider(
+          "https://rpc.testnet.moonbeam.network"
+        );
+        setLoadingMessage("Initializing Biconomy ...");
+        const biconomy = new Biconomy(window.ethereum, {
+          apiKey: config.apiKey.test,
+          debug: true,
+        });
 
-          biconomy.onEvent(biconomy.READY, () => {
+        // This web3 instance is used to read normally and write to contract via meta transactions.
+        web3 = new Web3(biconomy);
+
+        // This web3 instance is used to get user signature from connected wallet
+        walletWeb3 = new Web3(window.ethereum);
+
+        biconomy
+          .onEvent(biconomy.READY, () => {
             // Initialize your dapp here like getting user accounts etc
             contract = new web3.eth.Contract(
               config.contract.abi,
@@ -83,12 +194,10 @@ function App() {
             provider.on("accountsChanged", function(accounts) {
               setSelectedAddress(accounts[0]);
             });
-          }).onEvent(biconomy.ERROR, (error, message) => {
+          })
+          .onEvent(biconomy.ERROR, (error, message) => {
             // Handle error while initializing mexa
           });
-        } else {
-           showErrorMessage("Please change the network in metamask to Mumbai Testnet");
-        }
       } else {
         showErrorMessage("Metamask not installed");
       }
@@ -96,7 +205,15 @@ function App() {
     init();
   }, []);
 
-  const onQuoteChange = event => {
+  const handleClose = () => {
+    setBackdropOpen(false);
+  };
+
+  const handleToggle = () => {
+    setBackdropOpen(!backdropOpen);
+  };
+
+  const onQuoteChange = (event) => {
     setNewQuote(event.target.value);
   };
 
@@ -105,7 +222,8 @@ function App() {
       setTransactionHash("");
       if (metaTxEnabled) {
         console.log("Sending meta transaction");
-        let privateKey = "2ef295b86aa9d40ff8835a9fe852942ccea0b7c757fad5602dfa429bcdaea910";
+        let privateKey =
+          "2ef295b86aa9d40ff8835a9fe852942ccea0b7c757fad5602dfa429bcdaea910";
         let userAddress = "0xE1E763551A85F04B4687f0035885E7F710A46aA6";
         let nonce = await contract.methods.getNonce(userAddress).call();
         let functionSignature = contract.methods.setQuote(newQuote).encodeABI();
@@ -117,31 +235,45 @@ function App() {
         const dataToSign = {
           types: {
             EIP712Domain: domainType,
-            MetaTransaction: metaTransactionType
+            MetaTransaction: metaTransactionType,
           },
           domain: domainData,
           primaryType: "MetaTransaction",
-          message: message
+          message: message,
         };
 
-        const signature = sigUtil.signTypedMessage(new Buffer.from(privateKey, 'hex'), {data: dataToSign}, 'V4');
+        const signature = sigUtil.signTypedMessage(
+          new Buffer.from(privateKey, "hex"),
+          { data: dataToSign },
+          "V4"
+        );
         let { r, s, v } = getSignatureParameters(signature);
-        let executeMetaTransactionData = contract.methods.executeMetaTransaction(userAddress, functionSignature, r, s, v).encodeABI();
+        let executeMetaTransactionData = contract.methods
+          .executeMetaTransaction(userAddress, functionSignature, r, s, v)
+          .encodeABI();
         let txParams = {
-          "from": userAddress,
-          "to": config.contract.address,
-          "value": "0x0",
-          "gas": "100000",
-          "data": executeMetaTransactionData
+          from: userAddress,
+          to: config.contract.address,
+          value: "0x0",
+          gas: "500000",
+          data: executeMetaTransactionData,
         };
-        const signedTx = await web3.eth.accounts.signTransaction(txParams, `0x${privateKey}`);
-        let receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction, (error, txHash) => {
-          if (error) {
+        const signedTx = await web3.eth.accounts.signTransaction(
+          txParams,
+          `0x${privateKey}`
+        );
+        let receipt = await web3.eth.sendSignedTransaction(
+          signedTx.rawTransaction,
+          (error, txHash) => {
+            if (error) {
               return console.error(error);
+            }
+            console.log("Transaction hash is ", txHash);
+            showInfoMessage(
+              `Transaction sent to blockchain with hash ${txHash}`
+            );
           }
-          console.log("Transaction hash is ", txHash);
-          showInfoMessage(`Transaction sent to blockchain with hash ${txHash}`);
-        });
+        );
         setTransactionHash(receipt.transactionHash);
         showSuccessMessage("Transaction confirmed");
         getQuoteFromNetwork();
@@ -162,9 +294,9 @@ function App() {
     } else {
       showErrorMessage("Please enter the quote");
     }
-  }
+  };
 
-  const onSubmit = async event => {
+  const onSubmit = async (event) => {
     if (newQuote != "" && contract) {
       setTransactionHash("");
       if (metaTxEnabled) {
@@ -180,20 +312,20 @@ function App() {
         const dataToSign = JSON.stringify({
           types: {
             EIP712Domain: domainType,
-            MetaTransaction: metaTransactionType
+            MetaTransaction: metaTransactionType,
           },
           domain: domainData,
           primaryType: "MetaTransaction",
-          message: message
+          message: message,
         });
-        console.log(domainData);
-        console.log();
-        web3.currentProvider.send(
+
+        // NOTE: Using walletWeb3 here, as it is connected to the wallet where user account is present.
+        walletWeb3.currentProvider.send(
           {
             jsonrpc: "2.0",
             id: 999999999999,
             method: "eth_signTypedData_v4",
-            params: [userAddress, dataToSign]
+            params: [userAddress, dataToSign],
           },
           function(error, response) {
             console.info(`User signature is ${response.result}`);
@@ -201,16 +333,6 @@ function App() {
               showErrorMessage("Could not get user signature");
             } else if (response && response.result) {
               let { r, s, v } = getSignatureParameters(response.result);
-              console.log(userAddress);
-              console.log(JSON.stringify(message));
-              console.log(message);
-              console.log(getSignatureParameters(response.result));
-
-              const recovered = sigUtil.recoverTypedSignature_v4({
-                data: JSON.parse(dataToSign),
-                sig: response.result
-              });
-              console.log(`Recovered ${recovered}`);
               sendSignedTransaction(userAddress, functionSignature, r, s, v);
             }
           }
@@ -234,7 +356,7 @@ function App() {
     }
   };
 
-  const getSignatureParameters = signature => {
+  const getSignatureParameters = (signature) => {
     if (!web3.utils.isHexStrict(signature)) {
       throw new Error(
         'Given value "'.concat(signature, '" is not a valid hex string.')
@@ -248,44 +370,55 @@ function App() {
     return {
       r: r,
       s: s,
-      v: v
+      v: v,
     };
   };
 
   const getQuoteFromNetwork = () => {
-    if (web3 && contract) {
-      contract.methods
-        .getQuote()
-        .call()
-        .then(function(result) {
-          console.log(result);
-          if (
-            result &&
-            result.currentQuote != undefined &&
-            result.currentOwner != undefined
-          ) {
-            if (result.currentQuote == "") {
-              showErrorMessage("No quotes set on blockchain yet");
+    setLoadingMessage("Getting Quote from contact ...");
+    try {
+      if (web3 && contract) {
+        contract.methods
+          .getQuote()
+          .call()
+          .then(function(result) {
+            handleClose();
+            console.log(result);
+            if (
+              result &&
+              result.currentQuote != undefined &&
+              result.currentOwner != undefined
+            ) {
+              if (result.currentQuote == "") {
+                showErrorMessage("No quotes set on blockchain yet");
+              } else {
+                setQuote(result.currentQuote);
+                setOwner(result.currentOwner);
+              }
             } else {
-              setQuote(result.currentQuote);
-              setOwner(result.currentOwner);
+              showErrorMessage(
+                "Not able to get quote information from Network"
+              );
             }
-          } else {
-            showErrorMessage("Not able to get quote information from Network");
-          }
-        });
+          });
+      } else {
+        handleClose();
+      }
+    } catch (error) {
+      handleClose();
+      console.log(error);
     }
   };
 
-  const showErrorMessage = message => {
+  const showErrorMessage = (message) => {
     NotificationManager.error(message, "Error", 5000);
   };
 
-  const showSuccessMessage = message => {
+  const showSuccessMessage = (message) => {
     NotificationManager.success(message, "Message", 3000);
   };
 
-  const showInfoMessage = message => {
+  const showInfoMessage = (message) => {
     NotificationManager.info(message, "Info", 3000);
   };
 
@@ -296,14 +429,10 @@ function App() {
           .executeMetaTransaction(userAddress, functionData, r, s, v)
           .estimateGas({ from: userAddress });
         let gasPrice = await web3.eth.getGasPrice();
-        console.log(gasLimit);
-        console.log(gasPrice);
         let tx = contract.methods
           .executeMetaTransaction(userAddress, functionData, r, s, v)
           .send({
             from: userAddress,
-            gasPrice:gasPrice,
-            gasLimit:gasLimit
           });
 
         tx.on("transactionHash", function(hash) {
@@ -323,6 +452,20 @@ function App() {
 
   return (
     <div className="App">
+      <section className="top-row">
+        <div className="top-row-item">
+          <span className="label">Library </span>
+          <span className="label-value">web3.js</span>
+        </div>
+        <div className="top-row-item">
+          <span className="label">Meta Transaction</span>
+          <span className="label-value">Custom Approach</span>
+        </div>
+        <div className="top-row-item">
+          <span className="label">Signature Type</span>
+          <span className="label-value">EIP712 Signature</span>
+        </div>
+      </section>
       <section className="main">
         <div className="mb-wrap mb-style-2">
           <blockquote cite="http://www.gutenberg.org/ebboks/11">
@@ -341,15 +484,19 @@ function App() {
         </div>
       </section>
       <section>
-        {transactionHash !== "" && <Box className={classes.root} mt={2} p={2}>
-          <Typography>
-            Check your transaction hash
-            <Link href={`https://mumbai-explorer.matic.today/tx/${transactionHash}`} target="_blank"
-            className={classes.link}>
-              here
-            </Link>
-          </Typography>
-        </Box>}
+        {transactionHash !== "" && (
+          <Box className={classes.root} mt={2} p={2}>
+            <Typography>
+              Check your transaction hash
+              <Link
+                href={`https://kovan.etherscan.io/tx/${transactionHash}`}
+                target="_blank"
+                className={classes.link}>
+                here
+              </Link>
+            </Typography>
+          </Box>
+        )}
       </section>
       <section>
         <div className="submit-container">
@@ -363,12 +510,23 @@ function App() {
             <Button variant="contained" color="primary" onClick={onSubmit}>
               Submit
             </Button>
-            <Button variant="contained" color="primary" onClick={onSubmitWithPrivateKey} style={{marginLeft: "10px"}}>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={onSubmitWithPrivateKey}
+              style={{ marginLeft: "10px" }}>
               Submit (using private key)
             </Button>
           </div>
         </div>
       </section>
+      <Backdrop
+        className={classes.backdrop}
+        open={backdropOpen}
+        onClick={handleClose}>
+        <CircularProgress color="inherit" />
+        <div style={{ paddingLeft: "10px" }}>{loadingMessage}</div>
+      </Backdrop>
       <NotificationContainer />
     </div>
   );

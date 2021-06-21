bf0e1f2fb81c436230960022f729e47fdb49883f
Viswanath K <viswanath.iit@gmail.com>
2021-06-21 16:53:00 +0530

    chore: update config to florencenet

--- src/config.js
+++ src/config.js
@@ -2,8 +2,8 @@ import { ChainIds } from "@taquito/taquito";
 import { NetworkType } from "@airgap/beacon-sdk";
 
 export const config = {
-  rpcUrl: "https://delphinet-tezos.giganode.io",
-  quoteContractAddress: "KT18kvCsp28PiXf1DhuqpqbHfStj7QbdkJYN",
-  network: NetworkType.DELPHINET.toString(),
-  chainId: ChainIds.DELPHINET.toString(),
+  rpcUrl: "https://florencenet.smartpy.io",
+  quoteContractAddress: "KT1KYk2qjHX4Um5DzMu5QShJKofAjnsb7QJk",
+  network: NetworkType.FLORENCENET.toString(),
+  chainId: ChainIds.FLORENCENET.toString(),
 };
